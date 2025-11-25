71f09266c818bb0b0d272d35ec4517b4fb729762
chiisen <chiisen@hotmail.com>
2025-11-25 09:51:00 +0800

    ✨feat: Add SQL queries for `activity_day` and `activity_day_of_first_monday`.

--- sql/activity_day_of_first_monday.sql
+++ sql/activity_day_of_first_monday.sql
@@ -1,5 +1,6 @@
 SELECT * 
 FROM alatech.activity_day_of_first_monday 
 WHERE user_id IN (##USER_ID##)
+AND year_num = ##YEAR##
 AND start_time >= '##TODAY##T00:00:00.000+08:00' 
 AND start_time <= '##TODAY##T23:59:59.999+08:00';
